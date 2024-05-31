0d4f0b7c2b127de91f91b55d927f5e52e1a1e8b8
takiido <157808651+takiido@users.noreply.github.com>
2024-05-30 21:42:38 -0500

    updated PlayerMovement

--- Assets/Core/Player/PlayerMovement.cs
+++ Assets/Core/Player/PlayerMovement.cs
@@ -7,16 +7,32 @@ namespace Core.Player
 {
     public class PlayerMovement : MonoBehaviour
     {
+        public float fwdSpeed = 10.0f;
+        public float laneChangeSpeed = 10.0f;
+        public float jumpForce = 10.0f;
+        public float slideDuration = 1.0f;
 
+        private Rigidbody _rb;
+        private bool _isJumping = false;
+        private bool _isSliding = false;
+        private float _sliderTimer;
+
+        private int _curLane = 1;
+        private Vector3 _targetPos;
+
+        private void Start()
+        {
+            _rb = GetComponent<Rigidbody>();
+            _targetPos = transform.position;
+        }
     
         private void Update()
         {
+            //if (GameManager.Instance.isGameOver) return;
         }
-    
-        public void OnMove(InputAction.CallbackContext context)
+
+        private void HandleInput()
         {
-            if (context.performed)
-                Debug.Log("Move");
         }
     }
 }
